d4947a5cd26ec099c2b87d8cf3471522e4249818
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-02-03 22:23:09 -0500

    Add logic to 'get unique majors' function

--- lib/set_operations.py
+++ lib/set_operations.py
@@ -5,4 +5,11 @@ def unique_majors(student_list):
     Return a set of unique student majors using set comprehension.
     Extract the major field from each student record.
     """
-    pass
+    
+    # use set comprehension to get unique majors
+        # major is 3rd (python 2nd) element
+    unique_majors = {
+        student[2] for student in student_list
+    }
+
+    return unique_majors
