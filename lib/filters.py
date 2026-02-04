20d2816712f38cff759a1f82915d25355c3b6dc8
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-02-03 22:17:57 -0500

    Add logic to 'filter by major' function

--- lib/filters.py
+++ lib/filters.py
@@ -7,4 +7,13 @@ def filter_students_by_major(student_list, major):
     - Check if a student's major matches the given major (case insensitive).
     - Return a new list containing only students that match.
     """
-    pass
+    
+    # filter list
+        # student major is 3rd (python 2nd) element
+        # compare lower case just in case the casing of student major and user input does not match
+    filtered_list = [
+        student for student in student_list
+        if student[2].lower() == major.lower()
+    ]
+
+    return filtered_list
